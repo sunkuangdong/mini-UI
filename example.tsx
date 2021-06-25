9ed18046d17c499939eb03aa567070dd82d27e7f
sunsunkuangdong <sunkuangdong@edianzu.com>
2021-06-25 15:05:07 +0800

    Route根据Link进行展示对应的example

---
+++ example.tsx
@@ -0,0 +1,44 @@
+import React from "react";
+import ReactDom from "react-dom";
+import { HashRouter as Router, Link, Route } from "react-router-dom";
+import "./example.scss"
+import Icon from "./lib/icons/icon"
+import IconExample from "./lib/icons/icon.example"
+import ButtonExample from "./lib/button.example"
+
+ReactDom.render((
+    <Router>
+        <div>
+            <header>
+                <div>
+                    logo
+                </div>
+            </header>
+            <div className="mini-ui-content">
+                {/* 左边侧边栏 */}
+                <aside>
+                    <h2>组件</h2>
+                    <ul>
+                        <li><Link to="/icon">Icon</Link></li>
+                        <li><Link to="/button">Button</Link></li>
+                    </ul>
+                </aside>
+                {/* 右边内容区域 */}
+                <main>
+                    <Route path={"/icon"} component={IconExample} ></Route>
+                    <Route path={"/button"} component={ButtonExample} ></Route>
+                </main>
+            </div>
+            <footer>
+                <div className="mini-ui-footer">
+                    <div>
+                        <Icon icon="gitHubSvg" />
+                    </div>
+                    <div>
+                        这是我的组件库
+                    </div>
+                </div>
+            </footer>
+        </div>
+    </Router>
+), document.querySelector("#root"))
---
+++ lib/icons/icon.example.tsx
@@ -0,0 +1,13 @@
+import React from "react"
+import Icon from "./icon"
+
+const IconExample: React.FunctionComponent = () => {
+    return (
+        <div>
+            <Icon icon="gitHubSvg"/>
+            <Icon icon="reactSvg"/>
+            <Icon icon="webpackSvg"/>
+        </div>
+    )
+}
+export default IconExample
