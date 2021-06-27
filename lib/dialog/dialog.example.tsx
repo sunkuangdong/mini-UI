b3d9922aa75640a86225fb2b6fb92c9c2e49a225
孙匡东 <769296817@qq.com>
2021-06-27 21:46:24 +0800

    完成标签的Dialog

--- lib/dialog/dialog.example.tsx
+++ lib/dialog/dialog.example.tsx
@@ -1,14 +1,56 @@
-import React, { useState } from 'react'
+import React, { Fragment, useState } from 'react'
 import Dialog from './dialog'
 
 const DialogExample: React.FunctionComponent = () => {
     const [x, setX] = useState(false)
+    const [y, setY] = useState(false)
+    const cancel = () => {
+        setX(false)
+    }
+    const ok = () => {
+        setX(false)
+    }
+    const close = () => {
+        setX(false)
+    }
     return (
         <div>
-            <Dialog visible={x} >
-                <strong>hi</strong>
-            </Dialog>
-            <button onClick={() => setX(!x)}>提示消息</button>
+            <div>
+                <h1>example 1</h1>
+                <Dialog visible={x} buttons={[
+                    <Fragment>
+                        <button onClick={() => ok()}>ok</button>
+                        <button onClick={() => cancel()}>cancel</button>
+                    </Fragment>
+                ]} onClose={() => { close() }}>
+                    <strong>hi</strong>
+                </Dialog>
+                <button onClick={() => setX(!x)}>提示消息</button>
+            </div >
+            <div>
+                <h1>example 2</h1>
+                <Dialog visible={y} closeMask={true} buttons={[
+                    <Fragment>
+                        <button onClick={() => setY(!y)}>ok</button>
+                        <button onClick={() => setY(!y)}>cancel</button>
+                    </Fragment>
+                ]} onClose={() => { setY(!y) }}>
+                    <strong>hi</strong>
+                </Dialog>
+                <button onClick={() => setY(!y)}>提示消息</button>
+            </div >
+            <div>
+                <h1>example 3</h1>
+                <Dialog visible={y} closeMask={true} buttons={[
+                    <Fragment>
+                        <button onClick={() => setY(!y)}>ok</button>
+                        <button onClick={() => setY(!y)}>cancel</button>
+                    </Fragment>
+                ]} onClose={() => { setY(!y) }}>
+                    <strong>hi</strong>
+                </Dialog>
+                <button onClick={() => setY(!y)}>提示消息</button>
+            </div >
         </div>
     )
 }
--- lib/dialog/dialog.tsx
+++ lib/dialog/dialog.tsx
@@ -1,37 +1,51 @@
-import React, { Fragment } from 'react'
+import React, { Fragment, ReactElement } from 'react'
+import ReactDOM from 'react-dom';
 import "./dialog.scss"
 import { scopedClassMaker } from "../helpers/classes"
 import Icon from '../icons/icon';
 
 interface DialogProps {
     visible: boolean
+    buttons?: Array<ReactElement>
+    onClose: React.MouseEventHandler
+    closeMask?: boolean
 }
 const scopedClass = scopedClassMaker("mini-dialog")
 const sc = scopedClass
 const Dialog: React.FunctionComponent<DialogProps> = (props) => {
-    const { visible, children } = props
+    const { visible, children, buttons, closeMask } = props
+    const onClose: React.MouseEventHandler = (e) => {
+        // 传递这个e单纯为了ts
+        props.onClose(e)
+    }
+    const onClickClose: React.MouseEventHandler = (e) => {
+        if (closeMask) {
+            props.onClose(e)
+        }
+    }
     return (
-        visible ?
-            <Fragment>
-                <div className={sc("mask")}></div>
-                <div className={"mini-dialog"}>
-                    <header className={sc("header")}>
-                        提示
-                        <div className={sc("close")}>
-                            <Icon icon="close" />
-                        </div>
-                    </header>
-                    <main className={sc("main")}>
-                        {children}
-                    </main>
-                    <footer className={sc("footer")}>
-                        <button>ok</button>
-                        <button>cancel</button>
-                    </footer>
-                </div>
-            </Fragment>
-            :
-            null
+        ReactDOM.createPortal((
+            visible ?
+                <Fragment>
+                    <div className={sc("mask")} onClick={onClickClose}></div>
+                    <div className={"mini-dialog"}>
+                        <header className={sc("header")}>
+                            提示
+                        <div className={sc("close")} onClick={onClose}>
+                                <Icon icon="close" />
+                            </div>
+                        </header>
+                        <main className={sc("main")}>
+                            {children}
+                        </main>
+                        <footer className={sc("footer")}>
+                            {buttons ? buttons.map((item, index) => React.cloneElement(item, { key: index })) : <div></div>}
+                        </footer>
+                    </div>
+                </Fragment>
+                :
+                null
+        ), document.body)
     )
 }
 
