0e8d2eba5ed0e8f104026833961566b7457b1d21
sunsunkuangdong <sunkuangdong@edianzu.com>
2021-06-30 15:32:00 +0800

    重构scopedClassMaker函数，单元测试通过

--- lib/helpers/classes.tsx
+++ lib/helpers/classes.tsx
@@ -9,24 +9,17 @@ const classes = function (...names: (string | undefined)[]) {
     return names.filter(Boolean).join(' ')
 }
 
-const scopedClassMaker = function (prefix: string) {
-    return function (name?: string | ClassToggles, options?: Options) {
+const scopedClassMaker = (prefix: string) =>
+    (name?: string | ClassToggles, options?: Options) =>
         // ClassToggles: {hasAside: true, active: false}
         // 需要得到 ['hasAside']，然后用'-'拼接我们的 prefix
-        let name2
-        let result
-        if (typeof name === 'string' || !name) {
-            name2 = name
-            result = [prefix, name2].filter(Boolean).join("-")
-        } else {
-            name2 = Object.entries(name).filter(kv => kv[1]).map(kv => kv[0])
-            result = name2.map(n => [prefix, n].filter(Boolean).join("-")).join(' ')
-        }
-        if (options && options.extra) {
-            return [result, options.extra].filter(Boolean).join(" ")
-        } else {
-            return result
-        }
-    }
-}
+        Object.entries(name instanceof Object ? name : { [(name as string)]: name })
+            .filter(kv => kv[1] !== false)
+            .map(kv => kv[0])
+            .map(n => [prefix, n]
+                .filter(Boolean)
+                .join("-"))
+            .concat(options && options.extra || [])
+            .join(' ')
+
 export { classes, scopedClassMaker }
