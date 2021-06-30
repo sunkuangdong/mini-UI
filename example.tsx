e9ea07ced6dacca10e46a7c8102127bb328e276f
sunsunkuangdong <sunkuangdong@edianzu.com>
2021-06-30 18:22:54 +0800

    完成样式调整

--- example.tsx
+++ example.tsx
@@ -7,18 +7,23 @@ import IconExample from "./lib/icons/icon.example"
 import ButtonExample from "./lib/button.example"
 import DialogExample from "./lib/dialog/dialog.example"
 import layoutExample from "./lib/layout/layout.example"
+import { Layout, Header, Aside, Content, Footer } from "./lib/layout/layout";
+const logo = require('./mini.png')
 
 ReactDom.render((
     <Router>
-        <div>
-            <header>
-                <div>
-                    logo
+        <Layout className="site-page">
+            <Header className='site-header'>
+                <div className='logo'>
+                    <img src={logo} width='48' height='48' alt="" />
+                    <span className='logo-text'>
+                        MINI
+                    </span>
                 </div>
-            </header>
-            <div className="mini-ui-content">
+            </Header>
+            <Layout>
                 {/* 左边侧边栏 */}
-                <aside>
+                <Aside className='site-aside'>
                     <h2>组件</h2>
                     <ul>
                         <li><Link to="/icon">Icon</Link></li>
@@ -26,17 +31,17 @@ ReactDom.render((
                         <li><Link to="/dialog">Dialog</Link></li>
                         <li><Link to="/layout">layout</Link></li>
                     </ul>
-                </aside>
+                </Aside>
                 {/* 右边内容区域 */}
-                <main>
+                <Content>
                     <Route path={"/icon"} component={IconExample} ></Route>
                     <Route path={"/button"} component={ButtonExample} ></Route>
                     <Route path={"/dialog"} component={DialogExample} ></Route>
                     <Route path={"/layout"} component={layoutExample} ></Route>
-                </main>
-            </div>
-            <footer>
-                <div className="mini-ui-footer">
+                </Content>
+            </Layout>
+            <Footer>
+                <div>
                     <div>
                         <Icon icon="gitHubSvg" />
                     </div>
@@ -44,7 +49,7 @@ ReactDom.render((
                         这是我的组件库
                     </div>
                 </div>
-            </footer>
-        </div>
+            </Footer>
+        </Layout>
     </Router>
 ), document.querySelector("#root"))
