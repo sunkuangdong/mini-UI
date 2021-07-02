09a12b58e93d9b08096b0ea264e2bb5446151242
sunsunkuangdong <sunkuangdong@edianzu.com>
2021-07-02 14:28:42 +0800

    展示组件库

--- lib/icons/importIcons.jsx
+++
@@ -1,8 +0,0 @@
-// 遍历导入所有的svg
-// require.context
-let importAll = (requireContext) => requireContext.keys().forEach(requireContext)
-
-try {
-    importAll(require.context('../../public/icons', true, /\.svg$/))
-} catch (error) {
-}
--- lib/index.tsx
+++
@@ -1,1 +0,0 @@
-export { default as Icon } from "./icons/icon"
